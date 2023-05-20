c3c26a9ae2dc06153c1cb2217a2bd8324af3d08b
dmytrochupryna1 <128016969+dmytrochupryna1@users.noreply.github.com>
2023-05-20 12:49:17 +0700

    exercises 11-13

---
+++ exercise_11.py
@@ -0,0 +1,29 @@
+print('How old are you?', end=' ')
+age = input()
+
+print('How tall are you?', end=' ')
+height = input()
+
+print('How much do you weigh?', end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+
+
+print('-----------')
+print('Registration Form')
+print('-----------------')
+
+print('What is your name?', end=' ')
+name = input()
+
+print('What is your age?', end=' ')
+age = input()
+
+print('Where are you from?', end=' ')
+location = input()
+
+print(f'Hello {name}, you are {age} years old and you are from {location}')
+
