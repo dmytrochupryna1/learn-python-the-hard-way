e62db6f451d7005be6fdf28a2e6f684971bbf0a9
dmytrochupryna1 <128016969+dmytrochupryna1@users.noreply.github.com>
2023-05-20 16:17:27 +0700

    exercises 17-19

---
+++ exercise_18.py
@@ -0,0 +1,4 @@
+def print_two(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+print_two("Dima", "Asya")
---
+++ exercise_19.py
@@ -0,0 +1,12 @@
+def beers_and_snacks(beer_count, snacks_count):
+    print(f"You have {beer_count} beers!")
+    print(f"You have {snacks_count} snacks!")
+
+
+
+beers_and_snacks(10, 20)
+
+beers = 1000
+snacks = 2000
+
+beers_and_snacks(beers, snacks)
